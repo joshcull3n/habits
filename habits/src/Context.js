095a727dc7a5286a2711341fdfb1dc57c3228192
joshcull3n <8888780+joshcull3n@users.noreply.github.com>
2023-09-14 12:56:26 -0700

    update default habits

--- habits/src/Context.js
+++ habits/src/Context.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from 'react';
 
 export const Context = React.createContext();
 export const ContextProvider = ({ children }) => {
+
+  var today = new Date();
+  var yesterday2 = new Date();
+  yesterday2.setDate(yesterday2.getDate()-2)
   var initHabits = [
-    { id: 0, body: 'floss', doneDates: [new Date('2023/9/1'), new Date('2023/9/3')] },
-    { id: 1, body: 'do 50 pushups', doneDates: [new Date('2023/8/28'), new Date('2023/9/1'), new Date('2023/9/6')] },
-    { id: 2, body: 'learn how to do the worm', doneDates: [new Date('2023/9/8')] },
-    { id: 3, body: 'swim', doneDates: [new Date('2023/9/4'), new Date('2023/9/6')] }
+    { id: 0, body: 'floss', doneDates: [today] },
+    { id: 1, body: 'do 50 pushups', doneDates: [] },
+    { id: 2, body: 'practice doing the worm', doneDates: [today, yesterday2] }
   ];
 
   function convertToYYYYMMDD(date) {
