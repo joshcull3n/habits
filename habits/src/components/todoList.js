7339e082fa3230af8f9254aaf612193f9fa4c803
josh <josh@gridspace.com>
2024-11-08 17:40:53 -0800

    wip planning

---
+++ habits/src/components/todoList.js
@@ -0,0 +1,32 @@
+import { useContext } from 'react'
+import { TodoContext } from './todoContext.js'
+
+const TaskItem = () => {
+  const Label = ( <></> )    // task title
+  const Age = ( <></> )      // task age
+  const DueDate = ( <></> )  // task due date
+  const Notes = ( <></> )    // task description/notes
+  const Category = ( <></> ) // task category
+
+  const CompleteBtn = ( <></> ) // complete button
+  const SnoozeBtn = ( <></> )   // snooze button
+  const DeleteBtn = ( <></> )   // delete button
+}
+
+const TaskList = () => {
+  const totalCounter = ( <></> )
+  const doneCounter = ( <></> )
+  const openCounter = ( <></> )
+
+
+
+  return (
+    <div>
+      <div className="todoGrid">
+      
+      </div>
+    </div>
+  )
+}
+
+export default TaskList;
