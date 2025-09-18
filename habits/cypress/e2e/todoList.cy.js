873a0c14fb0022f13c7e15c32811a859ffe69dc0
joshcull3n <8888780+joshcull3n@users.noreply.github.com>
2025-09-18 14:30:41 -0700

    test scaffold

---
+++ habits/cypress/e2e/todoList.cy.js
@@ -0,0 +1,50 @@
+describe('sanity checks', () => {
+  it('login', () => {
+
+  })
+
+  it('logout', () => {
+
+  })
+
+  it('create account', () => {
+
+  })
+
+  it('joshcullen.co link', () => {
+
+  })
+
+  it('switch from dark to light mode', () => {
+
+  })
+
+  it('add task', () => {
+
+  })
+
+  it('add task with new list', () => {
+
+  })
+
+  it('add task to existing list', () => {
+
+  })
+
+  it('delete task', () => {
+
+  })
+
+  it('archive task', () => {
+
+  })
+
+  it('filter tasks by list', () => {
+
+  })
+
+  it('filter tasks by text filter', () => {
+
+  })
+
+})
